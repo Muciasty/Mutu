a6f60294084664cb2658d107c82f6bd7ffe9e52f
Muciasty <39488832+Muciasty@users.noreply.github.com>
2018-05-21 17:47:22 +0200

    Add files via upload

---
+++ WirSindPraktikanten_1/slider.js
@@ -0,0 +1,22 @@
+var numer = Math.floor(Math.random()*4)+1;
+			
+		
+			
+			function schowaj()
+			{
+				$("#slider").fadeOut(500);
+			}
+		
+			function zmienslajd()
+			{
+				numer++; if (numer>4) numer=1;
+				
+				var plik = "<img src=\"slajdy/min" + numer + ".jpg\" />";
+				
+				document.getElementById("slider").innerHTML = plik;
+				$("#slider").fadeIn(500);
+				
+				setTimeout("zmienslajd()", 5000);
+				setTimeout("schowaj()", 4500);
+			
+			}
